1c8bdb50622e249949691d09befa668a45a8ccad
Kanchan kumar Sharma <70506833+kanchankumar-sketch@users.noreply.github.com>
2021-07-14 22:20:14 +0530

    Add files via upload

---
+++ Employee_CRUD/src/main/java/com/empcrud/emp/controller/EmployeeResource.java
@@ -0,0 +1,55 @@
+package com.empcrud.emp.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.empcrud.emp.Employee.Employee;
+import com.empcrud.emp.service.EmployeeService;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeResource {
+	private final EmployeeService employeeService;
+	public EmployeeResource(EmployeeService employeeService)
+	{
+		this.employeeService=employeeService;
+	}
+	
+	@GetMapping("/all")
+	public ResponseEntity<List<Employee>> getAllEmployees()
+	{
+		List<Employee> employee=employeeService.findAllEmployees();
+		return new ResponseEntity<>(employee,HttpStatus.OK);
+	}
+	
+	@GetMapping("/find/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id)
+	{
+		Employee employee=employeeService.findEmployeeById(id);
+		return new ResponseEntity<>(employee,HttpStatus.OK);
+	}
+	
+	@PostMapping("/add")
+	public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee)
+	{
+		Employee newEmployee=employeeService.addEmployee(employee);
+		return new ResponseEntity<>(newEmployee,HttpStatus.OK);
+	}
+	
+	@PutMapping("/update")
+	public ResponseEntity<Employee> updateEmployeeById(@RequestBody Employee employee)
+	{
+		Employee updateEmployee=employeeService.updateEmployee(employee);
+		return new ResponseEntity<>(updateEmployee,HttpStatus.OK);
+	}
+}
